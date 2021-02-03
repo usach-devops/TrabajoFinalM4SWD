2fe3ed2ba797d41cfafd8436abceb23eac980084
Cristian Martinez <cristian.martinez.perez@gmail.com>
2021-02-02 23:28:01 -0300

    deteccion de driver y se ejecuta headless el browser

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -22,8 +23,25 @@ public class AppTest
     @Before
     public void setUp(){
         System.out.println("Iniciando configuración...");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+
+        switch (Util.getOS()) {
+            case WINDOWS:
+                //do windows stuff
+                System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+                break;
+            case LINUX:
+                System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+                 break;
+             default:
+             break;
+        }
+
+        //System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        
+        driver = new ChromeDriver(options);
         driver.get("https://www.amazon.com");
         driver.manage().window().maximize();
         System.out.println(driver.getCurrentUrl());
@@ -32,21 +50,25 @@ public class AppTest
     }
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws InterruptedException
     {
         System.out.println("Iniciando Pruebas...");
+        Thread.sleep(2000);
         WebElement searchbox = driver.findElement(By.name("q"));
+        Thread.sleep(2000);
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
+        Thread.sleep(2000);
 
         /*WebElement libro = driver.findElement(By.xpath("//*[@id=\"hdtb-msb-vis\"]/div[5]/a"));
         libro.click();*/
 
         WebElement compra = driver.findElements(By.className("LC20lb")).get(0);
-
+        Thread.sleep(2000);
         compra.click();
+        Thread.sleep(2000);
 
- 
+        System.out.println("El titulo es: " + driver.getTitle());
 
         assertEquals("Amazon.com: The DevOps Handbook: How to Create World-Class Agility, Reliability, and Security in Technology Organizations (9781942788003): Kim, Gene, Debois, Patrick, Willis, John, Humble, Jez, Allspaw, John: Books", driver.getTitle());
     }
